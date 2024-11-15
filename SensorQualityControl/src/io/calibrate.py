f8ee6f0fa5384f081216f6a9d1e791878a049f6e
Paul MacNichol <paulmacnichol@gmail.com>
2024-11-15 11:23:53 -0500

    fixing dark field

--- SensorQualityControl/src/io/calibrate.py
+++ SensorQualityControl/src/io/calibrate.py
@@ -59,7 +59,7 @@ def generate_dark_field_image():
     Generates a synthetic dark field image with uniform intensity.
     """
     print("Capturing flat field images...")
-    scope.led_on(sate=2)
+    scope.led_off()
     frame = cam.capture_image("dark_field_image", calibration=True)
     # Convert image to float for accurate division and avoid overflow
     image = frame.astype(np.float32)
