857fe3d0346403f09e211847ca22fd1b7e48b26a
Alexander J. Ross <alexander.ross@qatchtech.com>
2024-11-16 01:32:48 -0500

    Adding 'stitch' processes to position tiles The code in 'stitch_test.py' is the example Adding .gitignore to not track '__pycache__'

---
+++ SensorQualityControl/src/stitch/process_common.py
@@ -0,0 +1,24 @@
+def CheckStd(raw, avg, std):
+
+    raw_x = raw[0]
+    raw_y = raw[1]
+    avg_x = avg[0]
+    avg_y = avg[1]
+    std_x = std[0]
+    std_y = std[1]
+
+    allowable_deviation_factor = 4
+
+    if (abs(avg_x - raw_x) <= 
+        allowable_deviation_factor * std_x):
+        check_x = True
+    else:
+        check_x = False
+
+    if (abs(avg_y - raw_y) <=
+        allowable_deviation_factor * std_y):
+        check_y = True
+    else:
+        check_y = False
+
+    return (check_x, check_y)
